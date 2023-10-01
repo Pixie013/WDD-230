db40469b3cff25d94be9d71b3e53c4914f6c1db8
Carol Ann Maxwell <C.maxwell021@gmail.com>
2023-10-01 10:11:50 +0200

    last

--- scripts/script.js
+++ scripts/script.js
@@ -5,3 +5,16 @@ hamButton.addEventListener('click', () => {
     navigation.classList.toggle('open');
     hamButton.classList.toggle('open');
 });
+
+document.addEventListener('DOMContentLoaded', () => {
+    const menuButton = document.getElementById('menu');
+    const icon = document.getElementById('icon');
+    menuButton.addEventListener('click', () => {
+        document.body.classList.toggle('dark-mode');
+        if (document.body.classList.contains('dark-mode')) {
+            icon.src = 'images/sun.png';
+        } else {
+            icon.src = 'images/moon.png';
+        }
+    });
+});
