870413e72c7744bf4eec4d7f3ac6b0bfefb43e9f
Carol Ann Maxwell <C.maxwell021@gmail.com>
2023-10-08 11:55:23 +0200

    update

--- scripts/script.js
+++ scripts/script.js
@@ -18,3 +18,15 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
 });
+
+
+function updatePageVisitCounter() {
+    let count = localStorage.getItem('visitCount') || 0;  // Retrieve the current count from localStorage
+    count++;  // Increment the count
+    localStorage.setItem('visitCount', count);  // Store the updated count back into localStorage
+
+    const counterElement = document.getElementById('pageVisits');  // Get the element where the count will be displayed
+    if (counterElement) {
+        counterElement.innerText = `Page Visits: ${count}`;  // Update the element's text with the new count
+    }
+}
