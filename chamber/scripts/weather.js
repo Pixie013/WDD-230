349e484d516b5b307c7669c42060b6d2afb94b8b
Pixie013 <131585760+Pixie013@users.noreply.github.com>
2023-10-20 16:08:19 +0200

    Update weather.js
    weather update

--- chamber/scripts/weather.js
+++ chamber/scripts/weather.js
@@ -1,10 +1,29 @@
-$.getJSON("https://api.openweathermap.org/data/2.5/weather?q=Johannesburg&units=metric&appid=ab85ba57bbbb423fb62bfb8201126ede", function(data) {
-    console.log(data);
-    var icon = "https://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
-    var temp = Math.floor(data.main.temp);
-    var weather = data.weather[0].main;
+// api url
+const api_url = "https://api.openweathermap.org/data/2.5/weather?q=Johannesburg&units=metric&appid=ab85ba57bbbb423fb62bfb8201126ede";
 
-    $('.icon').attr('src', icon);
-    $(".temp").append(temp + ' &#8451;'); 
-    $(".weather").append(weather);
-});
+// Calling that async function
+getapi(api_url);
+
+// Defining async function
+async function getapi(url) {
+    
+    const response = await fetch(url);
+   
+    // Storing data in form of JSON
+    var data = await response.json();
+    
+    // console.log(data);
+    var tempStatus = Math.floor(data.main.temp);
+    var cityName = data.name+', '+data.sys.country;
+    var tempStatusMin = Math.floor(data.main.temp_min);
+    var tempStatusMax = Math.floor(data.main.temp_max);
+    var icon = "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
+
+    var weatherStatus = data.weather[0].description;
+    
+    document.getElementById('weather').innerHTML = weatherStatus;
+    document.getElementById('temp').innerHTML = tempStatusMin;
+    document.getElementById('icon').innerHTML = icon;
+    document.getElementById('cityName').innerHTML = cityName;
+    $('#icon').attr('src', icon);
+}
