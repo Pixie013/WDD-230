0e78950dac17c235de09460e6ce1b9215fffb587
Carol Ann Maxwell <C.maxwell021@gmail.com>
2023-10-22 17:31:58 +0200

    chances

--- chamber/scripts/lazyload.js
+++ chamber/scripts/lazyload.js
@@ -26,12 +26,13 @@ document.addEventListener("DOMContentLoaded", () => {
     images.forEach(image => { 
         imgObserver.observe(image)
     });
-})
+});
 
-
-
- @param {object} img //Image tag element //
-
+/**
+ * Preloads an image by updating its src attribute from the data-src attribute.
+ *
+ * @param {object} img - The image element to preload.
+ */
 function preloadImage(img) {
     // Get the new source attribute
     const src = img.getAttribute("data-src");
